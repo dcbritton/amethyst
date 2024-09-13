53f355a5dcaf4183305811acde4b69f7a79de5b9
dcbritton <122560490+dcbritton@users.noreply.github.com>
2024-09-12 22:41:25 -0400

    Refactor semantic analyzer helper methods. Add  more operator overload semantic analysis

--- src/SemanticAnalyzerVisitor.hpp
+++ src/SemanticAnalyzerVisitor.hpp
@@ -5,7 +5,6 @@
 
 #include <unordered_set>
 #include <unordered_map>
-#include <optional>
 #include "Visitor.hpp"
 
 struct SemanticAnalyzerVisitor : Visitor {
@@ -41,31 +40,30 @@ struct SemanticAnalyzerVisitor : Visitor {
             : name(name), signature(signature), returnType(returnType), parameters(parameters) {}
     };
 
-    // @TODO: rewrite using hashes of some kind
     struct OperatorOverload {
-
         // the operator and parameter (rhs) type are used to differentiate operators of a type, return type is not
         std::string op;
         std::string rhsType;
         std::string returnType;
 
-        // overload equality
-        bool operator==(const OperatorOverload& other) {
-            return (this->op == other.op && this->rhsType == other.rhsType);
-        }
-
         // constructor
         OperatorOverload(const std::string& op, const std::string& rhsType, const std::string& returnType)
             : op(op), rhsType(rhsType), returnType(returnType) {}
+
+        // overload equality
+        bool operator==(const OperatorOverload& other) {
+            return this->op == other.op && this->rhsType == other.rhsType;
+        }
     };
 
     struct Type {
         std::string name;
         std::vector<Variable> members;
         std::vector<Function> methods;
+        // @TODO: rewrite using a hash structure
         std::vector<OperatorOverload> overloads;
 
-        Type(const std::string& name, const std::vector<Variable>& members, const std::vector<Function>& methods, const std::vector<OperatorOverload>& overloads)
+        Type(const std::string& name, const std::vector<Variable>& members, const std::vector<Function>& methods, const std::vector<OperatorOverload> & overloads)
             : name(name), members(members), methods(methods), overloads(overloads) {}
 
         bool has(const OperatorOverload& opOverload) {
@@ -76,16 +74,15 @@ struct SemanticAnalyzerVisitor : Visitor {
     struct Scope {
         ScopeType type = undefined;
         std::unordered_map<std::string, Variable> variables;
-        // name of function/type for definition
-        std::optional<std::string> name;
+        std::string name; // name of function/type for definition
 
-        // constructor for type and function/method definitions
-        Scope(ScopeType type, std::optional<std::string> name)
+        // constructor
+        Scope(ScopeType type, const std::string& name)
             : type(type), name(name) {}
     };
 
-    // contains variables at all scopes
-    std::vector<Scope> variableTable;
+    // stack containing variables at all scopes
+    std::vector<Scope> scopeTable;
     // contains available types (types are only declarable in global)
     std::unordered_map<std::string, Type> types;
     // contains available functions (functions are only declarable in global)
@@ -94,47 +91,104 @@ struct SemanticAnalyzerVisitor : Visitor {
     std::vector<std::string> exprTypes;
 
     bool inFunction() {
-        for (const auto& scope : variableTable) {
-            if (scope.type == functionDefn) {
+        for (auto it = scopeTable.begin(); it != scopeTable.end(); ++it) {
+            if (it->type == functionDefn) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // fails if not in a function defn
+    std::vector<Scope>::const_iterator getFunctionDefnScope() {
+        auto it = scopeTable.begin();
+        for (; it != scopeTable.end(); ++it) {
+            if (it->type == functionDefn) {
+                return it;
+            }
+        }
+        
+        std::cout << "Internal error. getFunctionDefnScope() called when not in a function definition.\n";
+        exit(1);
+    }
+
+    bool inTypeDefn() {
+        for (auto it = scopeTable.begin(); it != scopeTable.end(); ++it) {
+            if (it->type == typeDefn) {
                 return true;
             }
         }
         return false;
     }
 
-    bool findVariable(const std::string& name) {
-        // functions don't capture external variables, so check only most recent function scope
-        if (inFunction()) {
-            if (variableTable.back().variables.find(name) != variableTable.back().variables.end()) {
-                return true;
+    // fails if not if a type defn
+    std::vector<Scope>::const_iterator getTypeDefnScope() {
+        auto it = scopeTable.begin();
+        for (; it != scopeTable.end(); ++it) {
+            if (it->type == typeDefn) {
+                return it;
+            }
+        }
+        std::cout << "Internal error. getTypeDefnScope() called when not in a type definition.\n";
+        exit(1); 
+    }
+
+    bool variableExists(const std::string& name) {
+        // anything in a type (methods, operators) should have access to (previously declared?) members, but nothing outside the type
+        if (inTypeDefn()) {
+            for (auto typeScope = getTypeDefnScope(); typeScope != scopeTable.end(); ++typeScope) {
+                if (typeScope->variables.find(name) != typeScope->variables.end()) {
+                    return true;
+                }
+            }
+        }
+        // functions don't capture external variables, so check only past the most recent function scope
+        else if (inFunction()) {
+            // get the function scope & check only past the most recent function scope
+            for (auto functionScope = getFunctionDefnScope(); functionScope != scopeTable.end(); ++functionScope) {
+                if (functionScope->variables.find(name) != functionScope->variables.end()) {
+                    return true;
+                }
             }
         }
         // otherwise, check all scopes
         else {
-            for (const auto& scope : variableTable) {
+            for (const auto& scope : scopeTable) {
                 if (scope.variables.find(name) != scope.variables.end()) {
                     return true;
                 }
             }
         }
-        
+
         return false;
     }
 
-    bool findFunction(const std::string& signature) {
+    bool functionExists(const std::string& signature) {
         return functions.find(signature) != functions.end();
     }
 
-    bool findType(const std::string& name) {
+    bool typeExists(const std::string& name) {
         return types.find(name) != types.end();
     }
 
+    void enterScope(ScopeType type, const std::string& name) {
+        scopeTable.push_back(Scope(type, name));
+    }
+
     void addToScope(Variable variable) {
-        variableTable.back().variables.emplace(variable.name, variable);
+        scopeTable.back().variables.emplace(variable.name, variable);
     }
 
     void endScope() {
-        variableTable.pop_back();
+        scopeTable.pop_back();
+    }
+
+    void printScopeTableNames() {
+        for (auto& scope: scopeTable) {
+            std::cout << scope.name << ", ";
+        }
+        std::cout << "\n";
     }
 
     void printExprStack() {
@@ -151,7 +205,7 @@ struct SemanticAnalyzerVisitor : Visitor {
     // visit program
     void visit(std::shared_ptr<Node::Program> n) override {
 
-        variableTable.push_back(Scope(global, std::nullopt));
+        enterScope(global, "global");
 
         // @TODO: predefined variables, functions, types may go here with addToScope()
         types.emplace("_int32", Type("_int32", {}, {}, {}));
@@ -171,13 +225,13 @@ struct SemanticAnalyzerVisitor : Visitor {
     // variable definition
     void visit(std::shared_ptr<Node::VariableDefn> n) override {
         // exists?
-        if (findVariable(n->name)) {
+        if (variableExists(n->name)) {
             std::cout << "In some scope, " << n->name << " is defined more than once.\n";
             exit(1);
         }
 
         // does n->type exist?
-        if (!findType(n->type)) {
+        if (!typeExists(n->type)) {
             std::cout << "The given type " << n->type
                       << " of variable " << n->name
                       << " has not yet been defined.\n";
@@ -213,7 +267,7 @@ struct SemanticAnalyzerVisitor : Visitor {
         }
 
         // return type check
-        if (!findType(n->returnType)) {
+        if (!typeExists(n->returnType)) {
             std::cout << "Return type " << n->returnType
                       << " of function " << n->name
                       << " has not yet been defined.\n";
@@ -221,14 +275,14 @@ struct SemanticAnalyzerVisitor : Visitor {
         }
 
         // name check 
-        if (findFunction(signature)) {
+        if (functionExists(signature)) {
             std::cout << "Function " << n->name << " is defined more than once with the same parameter types.\n"
                       << "(Mangled: " << signature << ")\n";
             exit(1);    
         }
         
         // make new scope
-        variableTable.push_back(Scope(functionDefn, n->name));
+        enterScope(functionDefn, n->name);
 
         // add the function (including to its own scope)
         functions.emplace(signature, Function(n->name, signature, n->returnType, parameters));
@@ -255,7 +309,7 @@ struct SemanticAnalyzerVisitor : Visitor {
     // visit parameter
     void visit(std::shared_ptr<Node::Parameter> n) override {
         // type exists?
-        if (!findType(n->type)) {
+        if (!typeExists(n->type)) {
             std::cout << "The given type " << n->type
                       << " of parameter " << n->name
                       << " has not yet been defined.\n";
@@ -267,14 +321,14 @@ struct SemanticAnalyzerVisitor : Visitor {
     void visit(std::shared_ptr<Node::Assignment> n) override {
 
         // exists?
-        if (!findVariable(n->lhs)) {
+        if (!variableExists(n->lhs)) {
             std::cout << "In some scope, tried to assign a value to " << n->lhs << ", which has not been defined.\n";
             exit(1);
         }
 
         // find type
         std::string type;
-        for (const auto& scope : variableTable) {
+        for (const auto& scope : scopeTable) {
             for (const auto& variable : scope.variables) {
                 if (n->lhs == variable.first)
                     type = variable.first;
@@ -303,7 +357,7 @@ struct SemanticAnalyzerVisitor : Visitor {
         n->expr->accept(shared_from_this());
         
         // match return type to expression type
-        std::string functionName = variableTable.back().name.value();
+        std::string functionName = getFunctionDefnScope()->name;
         std::string functionType = functions.find(functionName)->second.returnType;
         if (functionType != exprTypes.back()) {
             std::cout << "In a return in the function " << functionName
@@ -426,14 +480,14 @@ struct SemanticAnalyzerVisitor : Visitor {
     // visit variable
     void visit(std::shared_ptr<Node::Variable> n) override {
         // exists?
-        if (!findVariable(n->name)) {
+        if (!variableExists(n->name)) {
             std::cout << "Variable " << n->name << " referenced in some scope, but it has not yet been defined.\n";
             exit(1);
         }
 
         // find type
         std::string type;
-        for (const auto& scope : variableTable) {
+        for (const auto& scope : scopeTable) {
             for (const auto& variable : scope.variables) {
                 if (n->name == variable.second.name)
                     type = variable.second.type;
@@ -462,7 +516,7 @@ struct SemanticAnalyzerVisitor : Visitor {
         }
 
         // function exists?
-        if (!findFunction(signature)) {
+        if (!functionExists(signature)) {
             std::cout << "Could not find a matching definition for call with signature " << signature << ".\n";
             exit(1);
         }
@@ -481,34 +535,70 @@ struct SemanticAnalyzerVisitor : Visitor {
     // visit type definition
     void visit(std::shared_ptr<Node::TypeDefn> n) override {
         // name check
-        if (findType(n->name)) {
+        if (typeExists(n->name)) {
             std::cout << "Type " << n->name << " is already defined.\n";
             exit(1);
         }
 
         // enter scope
-        variableTable.push_back(Scope(typeDefn, n->name));
+        enterScope(typeDefn, n->name);
 
         // @TODO: process members
-        
-        // process overloads
-        for (const auto& overload : n->opOverloads) {
-
-            
-            overload->accept(shared_from_this());
-        }
 
         // @TODO: process methods
+
+        // process overloads
+        // @NOTE: violates visitor pattern
+        std::vector<OperatorOverload> overloads;
+        for (const auto& overload : n->opOverloads) {
+            // ensure no repeats
+            auto candidate = OperatorOverload(overload->op, overload->parameter->type, overload->returnType);
+            if (std::find(overloads.begin(), overloads.end(), candidate) != overloads.end()) {
+                std::cout << "Type " << n->name << "'s overload of op" << overload->op << "(:" << overload->parameter->type << "): is defined more than once!\n";
+                exit(1);
+            }
+            overloads.push_back(candidate);
+
+            // process each overload similarly to function
+            overload->accept(shared_from_this());
+        }
 
         // end scope
         endScope();
 
         // add
-        types.emplace(n->name, Type(n->name, {}, {}, {}));
+        types.emplace(n->name, Type(n->name, {}, {}, std::move(overloads)));
     }
 
     // visit operator overload
-    void visit(std::shared_ptr<Node::OperatorOverload> n) override {}
+    void visit(std::shared_ptr<Node::OperatorOverload> n) override {
+        
+        // process parameter
+        n->parameter->accept(shared_from_this());
+
+        // get typename
+        std::string typeName = getTypeDefnScope()->name;
+
+        // return type check
+        if (!typeExists(n->returnType)) {
+            std::cout << "Return type " << n->returnType
+                      << " of type " << typeName << "'s overload of op" << n->op << "(:" << n->parameter->type
+                      << "): has not yet been defined.\n";
+            exit(1);
+        }
+        
+        // make new scope
+        enterScope(overloadDefn, n->op + "(:" + n->parameter->type + "):");
+        
+        // add parameter to scope
+        addToScope(Variable(n->parameter->name, n->parameter->type));
+
+        // process function body
+        n->stmts->accept(shared_from_this());
+
+        // end its own scope, remove itself & parameters
+        endScope();    
+    }
 
     void visit(std::shared_ptr<Node::ConditionalBlock> n) override {}
 
